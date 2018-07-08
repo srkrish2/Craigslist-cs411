c3aa53abcd0c7e664fba3a7f448f3ceecfb3d4a8
Sneha K <srkrish2@illinois.edu>
2018-07-08 16:51:42 -0500

    reorg

--- search.php
+++ search.php
@@ -1,48 +1,9 @@
 <html>
 <head>
-	<title>Craigslist++</title>
-	<!-- jQuery CDN -->
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-		integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" 
-		crossorigin="anonymous"></script>
-	<!-- Bootstrap CDNs -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" 
-		integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" 
-		crossorigin="anonymous"></script>
-	<link	rel="stylesheet" 
-		href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
-		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-		crossorigin="anonymous">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
-		integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" 
-		crossorigin="anonymous"></script>
+	<?php include('./header_stuff.php'); ?>
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	  <a class="navbar-brand" href="#">Craigslist++</a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-	    <span class="navbar-toggler-icon"></span>
-	  </button>
-	  <div class="collapse navbar-collapse" id="navbarNav">
-	    <ul class="navbar-nav mr-auto">
-	      <li class="nav-item">
-		<a class="nav-link" href="#">Home</a>
-	      </li>
-	      <li class="nav-item active">
-		<a class="nav-link" href="#">Search<span class="sr-only">(current)</span></a>
-	      </li>
-	      <li class="nav-item">
-		<a class="nav-link" href="#">Manage</a>
-	      </li>
-	      <li class="nav-item">
-		<a class="nav-link" href="#">Inbox</a>
-	      </li>
-	    </ul>
-	    <span class="navbar-text">
-		Welcome, User1!
-	    </span>
-	  </div>
-	</nav>
+	<?php include('./nav_bar.php'); ?>
 	<div class="container-fluid">
 	<div class="row">
 		<div class="col-3" style="background-color:#13294b; height: 100%">
