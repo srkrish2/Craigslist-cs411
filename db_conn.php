d22a74783a30c91c4fdb316318ab121cf0acb0b3
Sneha K <srkrish2@illinois.edu>
2018-07-08 16:29:31 -0500

    reorg

---
+++ db_conn.php
@@ -0,0 +1,10 @@
+<?php
+
+function connect_to_db(){
+	$conn = mysqli_connect("localhost","craigslist","Password!234","track1");
+	if(!$conn) {
+		die("DB connection failed: ".mysqli_connect_error());
+	}
+}
+
+?>
